456234bd1b3694fac21d5ac3b772d9b16ac6e95c
angus croll <angus@twitter.com>
2015-05-06 08:12:43 -0700

    for-of

--- tests/iterators.spec.js
+++ tests/iterators.spec.js
@@ -22,22 +22,38 @@ describe('iterators', () => {
     });
   });
   describe('for-of', () => {
-    const iterables = [
+    const arrayLikeIterables = [
       [1,2,3],
-      // [1,2,3].keys(),
-      // [1,2,3].entries(),
       'abc',
-      // new Map([[1, 'one'], [2, 'two']]),
-      // new Set([1, 2, 2, 3])
-      // document.body
     ];
     let count;
-    it('iterables have a `Symbol.iterator` method', () => {
-      iterables.forEach((iterable) => {
+    it('works for arrayLikeIterables', () => {
+      arrayLikeIterables.forEach((iterable) => {
         count = 0;
         for (let x of iterable) {
-          console.log(x, count, iterable[count], iterable);
           assert.equal(x, iterable[count++]);
+        }
+      });
+    });
+    const mapLikeIterables = [
+      [1,2,3].keys(),
+      [1,2,3].entries(),
+      new Map([[1, 'one'], [2, 'two']]),
+      new Set([1, 2, 2, 4])/*,
+      document.body */
+    ];
+    it('works for mapLikeIterables', () => {
+      mapLikeIterables.forEach((iterable) => {
+        let toArray = [...iterable];
+        count = 0;
+        for (let x of iterable) {
+          console.log(x);
+          console.log(toArray[count]);
+          if (typeof x == 'object') {
+            assert.sameMembers(x, toArray[count++]);
+          } else {
+            assert.equal(x, toArray[count++]);
+          }
         }
       });
     });
