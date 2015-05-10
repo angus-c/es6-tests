2a0d6965fefb175bbbb410efa9f9191e39d88d8b
angus croll <angus@twitter.com>
2015-05-10 15:28:51 -0700

    Math

--- tests/Math.spec.js
+++ tests/Math.spec.js
@@ -14,11 +14,35 @@ describe('new Math features', () => {
       assert.equal(Math.cbrt(-8), -2);
     });
     it("has Math.log2 and Math.log10", () => {
-      assert.equal(Math.log2(2), 1);
+      assert.equal(Math.log2(8), 3);
       assert.equal(Math.log10(100), 2);
     });
-
-
+    it("has Math.fround", () => {
+      assert.notEqual(Math.fround(1.1), 1.1);
+      assert.equal(Math.round(Math.fround(1.1)), Math.round(1.1));
+    });
+    it("has Math.imul", () => {
+      assert.notEqual(Math.imul(5, 3.01), 5*3.01);
+      assert.equal(Math.round(Math.imul(5, 3.01)), Math.round(5*3.01));
+    });
+    it("has Math.clz32", () => {
+      assert.equal(Math.clz32(58), 26);
+    });
+    it("has Math.expm1", () => {
+      assert.closeTo(Math.expm1(1), 1.7182, 0.0001);
+    });
+    it("has Math.hypot", () => {
+      assert.equal(Math.hypot(3,4), 5);
+      assert.closeTo(Math.hypot(7,12), 13.8924, 0.0001);
+    });
+    it("has the new trig functions", () => {
+      assert.closeTo(Math.sinh(1), 1.1752, 0.0001);
+      assert.closeTo(Math.cosh(1), 1.5430, 0.0001);
+      assert.closeTo(Math.tanh(1), 0.7615, 0.0001);
+      assert.closeTo(Math.asinh(1), 0.8813, 0.0001);
+      assert.closeTo(Math.acosh(10), 2.9932, 0.0001);
+      assert.closeTo(Math.atanh(0.5), 0.5493, 0.0001);
+    });
   });
   describe('the properties', () => {
 
