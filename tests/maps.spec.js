80acb4b0c9e55ff6de45712f7071ced3f736990a
angus croll <angus@twitter.com>
2015-05-16 19:48:19 -0700

    map complete

--- tests/maps.spec.js
+++ tests/maps.spec.js
@@ -9,6 +9,10 @@ describe('maps', () => {
     });
   });
   describe('constructor parameter', () => {
+    it('accepts null or nothing', () => {
+      assert.isDefined(new Map(null));
+      assert.isDefined(new Map());
+    });
     it('accepts any key-value iterable', () => {
       const keyValueIterables = [
         [['a', 1], ['b', 2]],
@@ -52,6 +56,137 @@ describe('maps', () => {
       keys.forEach((key, i) => {
         assert.equal(map.get(keys[i]), values[i]);
       });
+    });
+  });
+  describe('`has` method', () => {
+    it('checks if key is present', () => {
+      const keys = [{}, [], 1, 'a', false, Symbol(), new Date()];
+      const values = [1, 'a', false, Symbol(), new Date(), {}, []];
+      let map = new Map();
+      keys.forEach((key, i) => {
+        map.set(key, values[i]);
+      });
+      keys.forEach((key, i) => {
+        assert.isTrue(map.has(keys[i]));
+      });
+    });
+  });
+  describe('map.keys()', () => {
+    let map, keys, values;
+    beforeEach(() => {
+      keys = [{}, [], 1, 'a', false, Symbol(), new Date()];
+      values = [1, 'a', false, Symbol(), new Date(), {}, []];
+      map = new Map();
+      keys.forEach((key, i) => {
+        map.set(key, values[i]);
+      });
+    });
+    it('defines `keys`', () => {
+      assert.equal(typeof map.keys, 'function');
+    });
+    it('(`map.keys`) is an iterator', () => {
+      const keysIt = map.keys();
+      assert.isDefined(keysIt.next);
+      keys.forEach((key) => {
+        assert.equal(keysIt.next().value, key);
+      });
+    });
+  });
+  describe('map.values()', () => {
+    let map, keys, values;
+    beforeEach(() => {
+      keys = [{}, [], 1, 'a', false, Symbol(), new Date()];
+      values = [1, 'a', false, Symbol(), new Date(), {}, []];
+      map = new Map();
+      keys.forEach((key, i) => {
+        map.set(key, values[i]);
+      });
+    });
+    it('defines `values`', () => {
+      assert.equal(typeof map.values, 'function');
+    });
+    it('(`map.values`) is an iterator', () => {
+      const valuesIt = map.values();
+      assert.isDefined(valuesIt.next);
+      values.forEach((value) => {
+        assert.equal(valuesIt.next().value, value);
+      });
+    });
+  });
+  describe('map.entries()', () => {
+    let map, keys, values;
+    beforeEach(() => {
+      keys = [{}, [], 1, 'a', false, Symbol(), new Date()];
+      values = [1, 'a', false, Symbol(), new Date(), {}, []];
+      map = new Map();
+      keys.forEach((key, i) => {
+        map.set(key, values[i]);
+      });
+    });
+    it('defines `entries`', () => {
+      assert.equal(typeof map.entries, 'function');
+    });
+    it('(`map.entries`) is an iterator', () => {
+      const entriesIt = map.entries();
+      assert.isDefined(entriesIt.next);
+      keys.forEach((key, i) => {
+        assert.sameMembers(entriesIt.next().value, [key, values[i]]);
+      });
+    });
+  });
+  describe('map.forEach', () => {
+    let map, keys, values;
+    beforeEach(() => {
+      keys = [{}, [], 1, 'a', false, Symbol(), new Date()];
+      values = [1, 'a', false, Symbol(), new Date(), {}, []];
+      map = new Map();
+      keys.forEach((key, i) => {
+        map.set(key, values[i]);
+      });
+    });
+    it('defines `forEach`', () => {
+      assert.equal(typeof map.forEach, 'function');
+    });
+    it('(`map.forEach`) loops through the entries', () => {
+      let count = 0;
+      map.forEach((value, key, mapp) => {
+        assert.equal(value, values[count]);
+        assert.equal(key, keys[count]);
+        assert.equal(mapp, map);
+        count++;
+      });
+    });
+  });
+  describe('map.size', () => {
+    it('(`map.size`) returns the number of map entries', () => {
+      let map = new Map([['a', 1], ['b', 2]]);
+      assert.equal(map.size, 2);
+      map.set('c', 3);
+      assert.equal(map.size, 3);
+    });
+  });
+  describe('map.delete()', () => {
+    it('defines `delete`', () => {
+      assert.equal(typeof new Map().delete, 'function');
+    });
+    it('(`map.delete`) deletes the specified key', () => {
+      let map = new Map([['a', 1], ['b', 2]]);
+      assert.equal(map.size, 2);
+      assert.isTrue(map.has('a'));
+      map.delete('a');
+      assert.equal(map.size, 1);
+      assert.isFalse(map.has('a'));
+    });
+  });
+  describe('map.clear()', () => {
+    it('defines `clear`', () => {
+      assert.equal(typeof new Map().clear, 'function');
+    });
+    it('(`map.clear`) empties the map', () => {
+      let map = new Map([['a', 1], ['b', 2]]);
+      assert.equal(map.size, 2);
+      map.clear();
+      assert.equal(map.size, 0);
     });
   });
 });
