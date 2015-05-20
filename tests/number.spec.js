9209a0932c3ec8dcfad641c5b6bdf8faa4a01416
angus croll <angus@twitter.com>
2015-05-19 21:47:19 -0700

    more numbers

--- tests/number.spec.js
+++ tests/number.spec.js
@@ -22,5 +22,21 @@ describe('new number features', () => {
       assert.equal(Number.isNaN('x'), false);
       assert.equal(Number.isNaN(NaN), true);
     });
+    it("has Number.isInteger", () => {
+      // assert.isFalse(Number.isInteger(0.5));
+      assert.isTrue(Number.isInteger(1));
+      assert.isFalse(Number.isInteger('q'));
+      assert.isFalse(Number.isInteger(NaN));
+    });
   });
+  describe('new static values', () => {
+    it("defines Number.EPSILON", () => {
+      // EPSILON is approx 2.2204460492503130808472633361816 x 10‍−‍16.
+      assert.isDefined(Number.EPSILON);
+      assert.isFalse((0.3 - (0.2 + 0.1)) === 0);
+      assert.isTrue((0.3 - (0.2 + 0.1)) < Number.EPSILON);
+      assert.isTrue((0.3 - (0.2 + 0.1)) > -Number.EPSILON);
+    });
+  });
+
 });
